95727616a937fc22b5fc9dff229ecf74e0793d68
Alex Spurling <alexspurling@gmail.com>
2011-12-25 00:15:11 +0000

    Implemeted randomiseList to get infinite list of random elements selected from input list

--- randomtree.hs
+++ randomtree.hs
@@ -1,34 +1,13 @@
 import System.Random
 import Ix
 
-randomText :: (Num a) => a -> a -> String
-randomText _ _ = "Hello"
-
-randomUpperCaseLetters gen = randomRs ('A','Z') gen
-
-randomLowerCaseLetters gen = randomRs ('a', 'z') gen
-
-randomDigits gen = randomRs ('0', '9') gen
-
-randomMerge (x:xs) (y:ys) gen =
-	let (randomBool, nextGen) = random gen :: (Bool, StdGen)
-	in if randomBool
-		then x:randomMerge xs ys nextGen
-		else y:randomMerge xs ys nextGen
-
+allowedChars = range ('A', 'Z') ++ range ('a', 'z') ++ range ('0', '9') ++ " ;"
 
 randomiseList (xs) gen =
-	let listLength = (length xs)-1
-	    (randomIndex, nextGen) = randomR (0, listLength) gen :: (Int, StdGen)
-	in (xs !! randomIndex) : randomiseList xs nextGen
-
---randomElem (xs) gen =
---    let maxIdx = (length xs)-1
---        range = (0 :: Int, length xs)
---        randomIndex = randomR range gen :: (Integer, StdGen)
---    in (xs !! randomIndex)
+     let listLength = (length xs)-1
+         (randomIndex, nextGen) = randomR (0, listLength) gen :: (Int, StdGen)
+         in (xs !! randomIndex) : randomiseList xs nextGen
 
 main = do
 	gen <- getStdGen
-	let charList = range ('A', 'Z') : range ('a', 'z') : range ('0', '9') : " ;"
-	putStrLn $ take 50 $ randomiseList charList gen
+	putStrLn $ take 5000 $ randomiseList allowedChars gen
